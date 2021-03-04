da63796fd7cf27ea610e48eab03930748a321379
philbro <philipbrodovsky@gmail.com>
2021-03-04 09:49:00 +0200

    js add event lisnter

---
+++ lessons/js-dom/index.js
@@ -0,0 +1,16 @@
+// callback if function as parameter
+//addEventListener((eventName: string), (callback: function));
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+	console.log('btn clicked');
+});
+
+const div = document.getElementById('div');
+div.addEventListener('click', onClick);
+// send onClick to addEventListener ,
+//after click browser run onCLick -  function onClick()
+
+function onClick() {
+	console.log('div onClick');
+}
